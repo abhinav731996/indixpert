35e0eec0539d2cf4e204a5be172fa2026d119761
abhinav731996 <abhinav731996@gmail.com>
2025-04-22 15:22:35 +0530

    c practice

--- clang/ex/excersices.c
+++ clang/ex/excersices.c
@@ -58,30 +58,87 @@
 //                   5.0L - 10.0L 20%
 //                   Above 10.0L 30%
 
+// #include<stdio.h>
+// int main()
+// {
+//     int income;
+//     float tax = 0;
+//     printf("Please enter your income: ");
+//     scanf("%d", &income);
+//     if (income<=250000) 
+//     {
+//         tax = 0;
+//     }
+//     else if (income>250000 && income<=500000)
+//     {
+//         tax = (income - 250000) * 0.05;
+//     }
+//     else if (income>500000 && income<=1000000)
+//     {
+//         tax = (500000 - 250000) * 0.05 + 0.2 * (income - 500000);
+//     }
+//     else{
+//         tax = (500000 - 250000) * 0.05 + 0.2 * (1000000 - 500000) + 0.3 * (income - 1000000);
+//     }
+    
+//     printf("the total tax you need to pay %.2f\n", tax);
+    
+//     return 0; 
+// }
+
+
+
+//+++++++++++++++ Student pass or fail +++++++++++++
+
+// #include<stdio.h>
+// int main()
+// {
+//     int marks1, marks2, marks3;
+//     printf("Enter marks1: \n");
+//     scanf("%d", &marks1);
+    
+//     printf("Enter marks2: \n");
+//     scanf("%d", &marks2);
+    
+//     printf("Enter marks3: \n");
+//     scanf("%d", &marks3);
+
+//     if (marks1<33 || marks2<33 || marks3<33)
+//     {
+//         printf("you are failed due to less marks in individual!!\n");
+//     }
+//     else if ((marks1+marks2+marks3)/3 <40)
+//     {
+//         printf("you are fail due to less percentge\n");
+//     }
+//     else
+//     {
+//         printf("you are pass!!\n");
+//     }    
+
+//     return 0;
+// }
+
+
+
+// +++++++++ Cal. leap year  ++++++++++
+
 #include<stdio.h>
 int main()
 {
-    int income;
-    int tax = 0;
-    printf("Please enter your income: ");
-    scanf("%d", &income);
-    if (income<=250000)
+    int year;
+    printf("Enter year: ");
+    scanf("%d", &year);
+
+    if (year %4==0 && year %100 !=0 || year%400 ==0)
     {
-        tax = 0;
+        printf("This is a leap year\n");
     }
-    else if (income>250000 && income<=500000)
+    else
     {
-        tax = (income - 250000) * 0.05;
-    }
-    else if (income>500000 && income<=1000000)
-    {
-        tax = (500000 - 250000) * 0.05 + 0.2 * (income - 500000);
-    }
-    else{
-        tax = (500000 - 250000) * 0.05 + 0.2 * (1000000 - 500000) + 0.3 * (income - 1000000);
+        printf("This is not a leap year\n");
     }
     
-    printf("the total tax you need to pay %d\n", tax);
     
     return 0;
 }
--- clang/ifelse/ifelse.c
+++ clang/ifelse/ifelse.c
@@ -37,53 +37,162 @@
 //         printf("Even number\n");
         
 //     }
-//         else
-//         {
-//             printf("Odd number\n");
-//         }
+//      else
+//      {
+//        printf("Odd number\n");
+//       }
     
 //     return 0;
 // }
 
-#include<stdio.h>
-int main ()
+
+
+
+// +++++++++++++++++++ Cal. (-, +, *, /) ++++++++++++++++++
+
+// #include<stdio.h>
+// int main ()
+// {
+//     int num1;
+//     printf("enter 1st numer: ");
+//     scanf("%d", &num1);
+    
+//     char operator;
+//     printf("enter any operation(-, +, *, /): ");
+//     scanf(" %c", &operator);                       // why do we add space before %c?
+
+//     int num2;
+//     printf("enter 2nd numer: ");
+//     scanf("%d", &num2);
+
+//     if (operator=='-')  
+//     {
+//         int sub = num1 - num2;
+//         printf("%d - %d = %d\n", num1, num2, sub);
+//     }
+//     else if (operator == '+')
+//     {
+//         int sum = num1 + num2;
+//         printf("%d + %d = %d\n", num1, num2, sum);
+//     }
+//     else if (operator == '/')
+//     {
+//         float div = (float)num1 / num2;
+//         printf("%d / %d = %.2f\n", num1, num2, div);
+//     }
+//     else if (operator == '*')
+//     {
+//         int mult = num1 * num2;
+//         printf("%d * %d = %d\n", num1, num2, mult);
+//     }
+//     else
+//     {
+//         printf("Please enter valid operation!!\n");
+//     }
+    
+//     return 0;
+// }
+
+
+
+// +++++++++++  fiond +ve & -ve number ++++++++++
+
+// #include<stdio.h>
+// int main()
+// {
+//     int userInput;
+//     printf("please enter any number: ");
+//     scanf("%d", &userInput);
+
+//     if (userInput>0)
+//     {
+//         printf("The number is positive\n");
+//     }
+//     else if (userInput<0)
+//     {
+//         printf("The number is negitive\n");
+//     }
+//     else{
+//         printf("Number is zero");
+//     }
+    
+//     return 0;
+// }
+
+
+
+
+//+++++++++ find is the number even or odd in b/w 0-100 +++++++++++
+
+// #include<stdio.h>
+// int main()
+// {
+//     int num;
+//     printf("please enter any number 0-100: ");
+//     scanf("%d", &num);
+
+//     if (num>0, num<100)
+//     {
+//         if (num%2==0)
+//         {
+//             printf("Your numbe is even number: %d", num);
+            
+//         }
+//         else
+//         {
+//             printf("Your number is odd: %d", num);n
+//         }
+        
+//     }
+
+//     return 0;
+// }
+
+
+
+//+++++++++++ Squar & Cube +++++++++++++++
+
+// #include<stdio.h>
+// int main()
+// {
+//     printf("1 for squar: \n");
+//     printf("2 for cube: \n");
+//     int option;
+//     printf("Please select any option: ");
+//     scanf("%d",&option);
+
+//     int num;
+//     printf("pleas enter any number: ");
+//     scanf("%d",&num);
+
+//     if (option==1)
+//     {
+//         printf("%d squar is: %d\n", num, num*num);
+//     }
+//     else if (option==2)
+//     {
+//         printf("%d cube is: %d\n", num, num*num*num);
+//     }
+//     else
+//     {
+//         printf("please select valid option");
+//     }
+
+//     return 0;
+// }
+
+
+#include<stdio.h>   
+int main()
 {
-    int num1;
-    printf("enter 1st numer: ");
-    scanf("%d", &num1);
-    
-    char operator;
-    printf("enter any operation(-, +, *): ");
-    scanf(" %c", &operator);                       // why do we add space before %c?
+    char ch;
 
-    int num2;
-    printf("enter 2nd numer: ");
-    scanf("%d", &num2);
-
-    if (operator=='-')  
+    for (ch = 'a'; ch <= 'z'; ch++)
     {
-        int sub = num1 - num2;
-        printf("%d - %d = %d\n", num1, num2, sub);
-    }
-    else if (operator == '+')
-    {
-        int sum = num1 + num2;
-        printf("%d + %d = %d\n", num1, num2, sum);
-    }
-    // else if (operator == '/')
-    // {
-    //     float div = num1 / num2;
-    //     printf("%.2f / %.2f = %.2f", num1, num2, div);
-    // }
-    else if (operator == '*')
-    {
-        int mult = num1 * num2;
-        printf("%d * %d = %d\n", num1, num2, mult);
-    }
-    else
-    {
-        printf("Please enter valid operation!!\n");
+        printf("%c\t ", ch);
     }
     
+
     return 0;
 }
+
