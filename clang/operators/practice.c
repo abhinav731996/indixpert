2af563956a507f2e39585f6d3403baa99e8b1890
Abhinav <abhinav731996@gmail.com>
2025-05-02 17:05:50 +0530

    practice

--- clang/operators/practice.c
+++ clang/operators/practice.c
@@ -1,36 +1,62 @@
-#include<stdio.h>
-int main ()
+// #include<stdio.h>
+// int main ()
+// {
+//     printf("please enter the following result: \n");
+
+//     int c;
+//     printf("c: ");
+//     scanf("%d", &c);
+
+//     int python;
+//     printf("Python: ");
+//     scanf("%d", &python);
+    
+//     int javascript;
+//     printf("Javascript: ");
+//     scanf("%d", &javascript);
+    
+//     int react;
+//     printf("React: ");
+//     scanf("%d", &react);
+    
+//     int node;
+//     printf("Node.js: ");
+//     scanf("%d", &node);
+
+//     if (c<70 || python<70 || javascript<70 || react<70 || node<70)
+//     {
+//         printf("Fail!!\n");
+//     }
+//     else{
+//         printf("Pass!!\n");
+//     }
+    
+    
+//     return 0;
+// }
+
+
+#include <stdio.h>
+
+int main()
 {
-    printf("please enter the following result: \n");
+    int num = 123456;
+    int reversed = 0;
 
-    int c;
-    printf("c: ");
-    scanf("%d", &c);
-
-    int python;
-    printf("Python: ");
-    scanf("%d", &python);
-    
-    int javascript;
-    printf("Javascript: ");
-    scanf("%d", &javascript);
-    
-    int react;
-    printf("React: ");
-    scanf("%d", &react);
-    
-    int node;
-    printf("Node.js: ");
-    scanf("%d", &node);
-
-    if (c<70 || python<70 || javascript<70 || react<70 || node<70)
+    // Reverse the number to print left to right
+    int temp = num;
+    while (temp > 0)
     {
-        printf("Fail!!\n");
+        reversed = reversed * 10 + (temp % 10);
+        temp /= 10;
     }
-    else{
-        printf("Pass!!\n");
+
+    // Print each digit from reversed number
+    while (reversed > 0)
+    {
+        printf("%d\n", reversed % 10);
+        reversed /= 10;
     }
-    
-    
+
     return 0;
 }
